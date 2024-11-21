e7399a9919e110fdda874ae8f882334a67355b26
AryanJadhav <jadhavaryan467@gmail.com>
2024-11-21 12:27:07 +0530

    efv

--- sitemap.php
+++ sitemap.php
@@ -6,6 +6,21 @@ header('Content-Type: application/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
+// Add static pages
+$static_pages = [
+    'http://localhost/adharpshy-1/index.php',
+    'http://localhost/adharpshy-1/contact.php',
+   
+];
+
+foreach ($static_pages as $page_url) {
+    echo '<url>';
+    echo '<loc>' . $page_url . '</loc>';
+    echo '<lastmod>' . date('Y-m-d') . '</lastmod>'; 
+    echo '<priority>1.0</priority>'; 
+    echo '</url>';
+}
+
 // Fetch all published blog posts
 $sql = "SELECT title FROM blogs WHERE is_enabled = 1"; 
 $result = mysqli_query($conn, $sql);
@@ -24,7 +39,6 @@ if ($result) {
         echo '</url>';
     }
 }
-
 
 echo '</urlset>';
 mysqli_close($conn);
